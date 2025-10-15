b39ef1b2c83b91f7fe659eea54a673cf1c098e4b
Capivara <danilo.duarte00700@gmail.com>
2025-10-15 19:37:49 -0300

    .

--- script2.js
+++ script2.js
@@ -1,8 +1,17 @@
 //botão hamburguer
 const Btn = document.getElementById('hamburguer')
 const Nav = document.getElementById('navbar')
+const Bloco_Principal = document.getElementById('Bloco-Principal')
+const Menu_lateral = document.getElementById('Menu-lateral')
 
 Btn.addEventListener('click', () => {
-    console.log("foi")
     Nav.classList.toggle("active");
+
+    if(Nav.classList.contains("active")){
+        Bloco_Principal.style.width = "80%"
+        Menu_lateral.style.marginLeft = "0"
+    } else {
+        Bloco_Principal.style.width = "100%"
+        Menu_lateral.style.marginLeft = "-20%"
+    }
 });
